711c82042db06d87520b74ad9a555e3874528934
kelly r maass <kmaass77627@gmail.com>
2020-04-14 06:08:21 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :students
-  get '/students/:id/activate' to: students#activate
+  get '/students/:id/activate' 
   # For details reson the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
