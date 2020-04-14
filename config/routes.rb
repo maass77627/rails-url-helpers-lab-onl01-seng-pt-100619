ade0ffe94b910d5e2cfd78540261e0c9a8c8e4e0
kelly r maass <kmaass77627@gmail.com>
2020-04-14 06:12:02 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :students, only: :index
 
   get "students/:id", to: "students#show", as: "student"
-  get "students/:id/activate", to: "students#activate", as: "activate_student"e within this file, see http://guides.rubyonrails.org/routing.html
+  get "students/:id/activate", to: "students#activate", as: "activate_student"
 end
