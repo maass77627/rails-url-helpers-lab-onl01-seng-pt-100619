eed0cb339840b8f26a6aa145553fd5f2d628c330
kelly r maass <kmaass77627@gmail.com>
2020-04-14 05:56:13 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :students 
+  resources :students
   # For details reson the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
